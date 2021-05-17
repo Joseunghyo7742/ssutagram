c0b7bb3bba4e3d391d706e23a4322a1ecf4f2033
joseunghyo <seunghyo7742@naver.com>
2021-05-18 00:59:44 +0900

    홈화면 모아보기 & detail.html

--- SSUtagram/SSUtagram/settings.py
+++ SSUtagram/SSUtagram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'SSUtagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['SSUtagram/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+os.path.join(BASE_DIR, 'main', 'static')]# static파일이 어디에 있는지
+STATIC_ROOT= os.path.join(BASE_DIR, 'static') # static파일을 어디에 모을건지
+MEDIA_ROOT=os.path.join(BASE_DIR,'meida') #이용자가 업로드한 media파일을 모으는 곳    
+MEDIA_URL='/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- SSUtagram/main/views.py
+++ SSUtagram/main/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Feed
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world!")
+def home(request):
+    feeds=Feed.objects.all()
+    return render(request, 'home.html',{'feeds':feeds})
+
+def detail(request, feed_id ):
+    feed=get_object_or_404(Feed,pk=feed_id)
+    return render(request,'detail.html',{'feed':feed})
